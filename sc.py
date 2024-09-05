3eaa8778442a641f0f4c3163111447f8bfb15d24
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-09-05 23:10:50 +0530

    Update sc.py
    importing the dependencies

--- sc.py
+++ sc.py
@@ -3,5 +3,9 @@ import numpy as np
 import pandas as pd
 import difflib
 
+from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
 df = pd.read_csv('Data/movies.csv')
 df.head()
