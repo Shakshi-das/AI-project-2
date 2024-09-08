07f23b309c8bf63280d2a97b6833d5abc275fbcd
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-09-08 23:59:16 +0530

    Update sc.py
    data pre processing

--- sc.py
+++ sc.py
@@ -6,6 +6,10 @@ import difflib
 from sklearn.feature_extraction.text import TfidVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#data collection and pre-processing
 
+#loading data from csv file
 df = pd.read_csv('Data/movies.csv')
 df.head()
+
+#printing the first 5 rows
