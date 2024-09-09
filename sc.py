e28c5913711c1162fbe4955dd7bfdb75111b85c9
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-09-09 23:45:42 +0530

    Update sc.py

--- sc.py
+++ sc.py
@@ -13,3 +13,8 @@ df = pd.read_csv('Data/movies.csv')
 df.head()
 
 #printing the first 5 rows
+
+# Setting up some parameters for the workbook
+
+pd.set_option('display.max_rows', 500)
+pd.options.display.max_columns = None
