14fc527dd35e1c2156e3aed576eee00d6cc9a62c
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-09-27 23:54:49 +0530

    Update sc.py
    creating a loop for displaying all movie names

--- sc.py
+++ sc.py
@@ -78,3 +78,8 @@ i=1
 for movie in sorted_similar_movies
   index= movie[0]
   title_from_index= movies_data[movies_data.index==index)['title'].values[0]
+  if (i<30):
+    print(i,'.',title_from_index)
+    i+=1
+
+movie_name= input("Enter your favourite movie name:")
