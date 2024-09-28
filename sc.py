7f40883cfec15212256531b2acdf0806be03cf45
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-09-28 23:30:55 +0530

    Update sc.py
    added final comments and changes

--- sc.py
+++ sc.py
@@ -1,4 +1,5 @@
 #source code
+
 #importing dependencies
 import numpy as np
 import pandas as pd
@@ -83,3 +84,20 @@ for movie in sorted_similar_movies
     i+=1
 
 movie_name= input("Enter your favourite movie name:")
+list_of_all_titles= movies_data['title'].tolist()
+find_close_match= difflib.get_close_matches(movie_name, list_of_all_tiltes)
+close_match= find_close_match[0]
+index_of_the_movie= movies_data[movies_data.title == close_match]['index'].values[0]
+similarity_score= list(enumerate(similarity[index_of_the_movie]))
+sorted_similar_movies= sorted(similarity_score, key= lambda x:x[1], reverse= True)
+
+print("Movies suggested for you: \n")
+
+i=1
+
+for movie in sorted_similar_movies
+  index= movie[0]
+  title_from_index= movies_data[movies_data.index==index)['title'].values[0]
+  if (i<30):
+    print(i,'.',title_from_index)
+    i+=1
